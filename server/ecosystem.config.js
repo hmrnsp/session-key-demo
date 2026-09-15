c40a40319dcfadc88e522bea19207337d20906a1
Rede <bloglayangan@gmail.com>
2026-09-15 14:21:43 +0800

    tambah ecosystem config

---
+++ server/ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'session-key-server',
+      script: 'src/index.js',
+      interpreter: 'node',
+      env: {
+        NODE_ENV: 'production',
+      },
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      autorestart: true,
+      max_restarts: 10,
+      restart_delay: 3000,
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      merge_logs: true,
+      time: true,
+    },
+  ],
+};
